a2f2cb6aa4c6eb177b0b2bd2445231f5b1dcba27
DhruvilMShah <65597284+DhruvilMShah@users.noreply.github.com>
2024-11-10 11:26:17 +0530

    Feature/rest impl from main (#3)
    * bringing up restaurant service
    
    * added auth to restaurant and making auth work while packaging as war

--- dataService/src/main/java/DataServiceApplication.java
+++ dataService/src/main/java/DataServiceApplication.java
@@ -1,5 +1,3 @@
-package com.group.an.restaurantService;
-
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
@@ -8,25 +6,22 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.group.an.dataService","com.group.an.restaurantService","com.group.an.authService"})
-@EnableMongoRepositories(basePackages = "com.group.an.dataService.repositories")
 @SecurityScheme(name = "jwtAuth", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
-public class RestaurantServiceApplication {
+@SpringBootApplication(scanBasePackages = {"com.group.an.dataService","com.group.an.authService"})
+public class DataServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantServiceApplication.class, args);
+		SpringApplication.run(DataServiceApplication.class, args);
 	}
+
 	@Bean
-	@Profile("auth")
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.info(new Info()
-						.title("RestaurantService API Documentation")
+						.title("DataService API Documentation")
 						.version("1.0")
-						.description("APIs for Managing Restaurants in the Food Delivery System"));
+						.description("APIs for Managing Data in the Food Delivery System"));
 	}
 
 }
--- restaurantService/src/main/java/com/group/an/restaurantService/RestaurantController.java
+++ restaurantService/src/main/java/com/group/an/restaurantService/RestaurantController.java
@@ -1,21 +1,129 @@
 // Importing package in this code module 
 package com.group.an.restaurantService;
 // Importing required classes 
-import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.ResponseBody; 
+import com.group.an.dataService.models.MenuItem;
+import com.group.an.dataService.models.Restaurant;
+import com.group.an.dataService.repositories.RestaurantRepository;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-// Annotation 
-@Controller
-// Main class 
-public class RestaurantController { 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.security.access.prepost.PreAuthorize;
 
-	@RequestMapping("/hello") 
-	@ResponseBody
+// Annotation
+@RestController
+@RequestMapping("/restaurants")
+@SecurityRequirement(name = "jwtAuth")
+public class RestaurantController {
 
-	// Method 
-	public String helloFromRestaurant() 
-	{ 
-		return "helloFromRestaurant"; 
-	} 
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+
+	@GetMapping
+	public List<Restaurant> getAllRestaurants() {
+		return restaurantRepository.findAll();
+	}
+
+	@PostMapping
+	@PreAuthorize("hasRole('ADMIN')")
+	public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
+		return restaurantRepository.save(restaurant);
+	}
+
+	@GetMapping("/{restaurantId}/menu")
+	public ResponseEntity<List<MenuItem>> getAllRestaurant(@PathVariable("restaurantId") Integer restaurantId) {
+		Optional<Restaurant> restaurant = restaurantRepository.findById(restaurantId);
+		if (restaurant.isPresent()) {
+			return ResponseEntity.ok(restaurant.get().getMenus());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PostMapping("/{restaurantId}/menu")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('RESTAURANT_OWNER')")
+	public ResponseEntity<Restaurant> addItemToMenuOfRestaurant(@RequestBody MenuItem item, @PathVariable("restaurantId") Integer restaurantId) {
+		// ToDo: Validate if restaurantId on the endpoint and the restaurantId of the logged in RestaurantOwner is same
+		Optional<Restaurant> restaurant = restaurantRepository.findById(restaurantId);
+		if (restaurant.isPresent()) {
+			List<MenuItem> existingMenuItems = restaurant.get().getMenus();
+			if(existingMenuItems == null) {
+				existingMenuItems = new ArrayList<>();
+			}
+			existingMenuItems.add(item);
+			restaurant.get().setMenus(existingMenuItems);
+			restaurantRepository.save(restaurant.get());
+			return ResponseEntity.ok(restaurant.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PostMapping("/{restaurantId}/menu/{menuItemId}")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('RESTAURANT_OWNER')")
+	public ResponseEntity<Restaurant> updateMenuItemOfRestaurant(@RequestBody MenuItem item,
+																  @PathVariable("restaurantId") Integer restaurantId,
+																  @PathVariable("menuItemId") Integer menuItemId) {
+		// ToDo: Validate if restaurantId on the endpoint and the restaurantId of the logged in RestaurantOwner is same
+		Optional<Restaurant> restaurant = restaurantRepository.findById(restaurantId);
+		if (restaurant.isPresent()) {
+			List<MenuItem> existingMenuItems = restaurant.get().getMenus();
+
+			if(existingMenuItems == null) {
+				existingMenuItems = new ArrayList<>();
+				existingMenuItems.add(item);
+			}
+			else {
+				existingMenuItems = replaceExistingItem(existingMenuItems, menuItemId,item);
+			}
+			restaurant.get().setMenus(existingMenuItems);
+			restaurantRepository.save(restaurant.get());
+			return ResponseEntity.ok(restaurant.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	private List<MenuItem> replaceExistingItem(List<MenuItem> existingMenuItems, Integer menuItemId, MenuItem item) {
+		for(MenuItem existingItem : existingMenuItems) {
+			if(existingItem.getMenuItemId() == menuItemId) {
+				existingMenuItems.remove(existingItem);
+				existingMenuItems.add(item);
+				break;
+			}
+		}
+		return existingMenuItems;
+	}
+
+	@DeleteMapping("/{restaurantId}/menu/{menuItemId}")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('RESTAURANT_OWNER')")
+	public ResponseEntity<Restaurant> deleteMenuItemOfRestaurant(@PathVariable("restaurantId") Integer restaurantId,
+																  @PathVariable("menuItemId") Integer menuItemId) {
+		// ToDo: Validate if restaurantId on the endpoint and the restaurantId of the logged in RestaurantOwner is same
+		Optional<Restaurant> restaurant = restaurantRepository.findById(restaurantId);
+		if (restaurant.isPresent()) {
+			List<MenuItem> existingMenuItems = restaurant.get().getMenus();
+
+			if(existingMenuItems == null || existingMenuItems.isEmpty()) {
+				return ResponseEntity.notFound().build();
+			}
+			else {
+				for(MenuItem existingItem : existingMenuItems) {
+					if(existingItem.getMenuItemId() == menuItemId) {
+						existingMenuItems.remove(existingItem);
+						break;
+					}
+				}
+			}
+			restaurant.get().setMenus(existingMenuItems);
+			restaurantRepository.save(restaurant.get());
+			return ResponseEntity.ok(restaurant.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
